ca257704b4cb94136ec75c8c5460b006f30f878e
karciss <cisneroscarolina869@gmail.com>
2026-09-16 18:39:11 -0400

    feat: muestra nombre de Carolina en el encabezado

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
 <template>
   <header class="encabezado">
-    <h1>PR_Testing</h1>
+    <h1>PR_Testing - Carolina</h1>
     <p class="subtitulo">Repositorio oficial de BolivianWorks</p>
   </header>
 
